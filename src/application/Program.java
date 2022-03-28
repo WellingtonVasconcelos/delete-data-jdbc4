599bd9a8fe1e98e40e31caecd29d63d6f0e6ad15
Wellington Vasconcelos <wltvasc@hotmail.com>
2022-03-28 17:17:42 -0300

    Delete data MYSql with JDBC

---
+++ src/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DbIntegrityException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Connection conn = null;
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+			//Atualizar dados
+			st = conn.prepareStatement(
+					"DELETE FROM department "
+					+ "WHERE "
+					+ "Id = ?");
+			
+			st.setInt(1, 2);
+			
+			int rowAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " +  rowAffected);
+		}
+		catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
+		
+	}
+}
